8470cb1cf55cc28bd1b8046d9e97c5dc780cd5c2
Benjamin Thomas <benjamin.thomas@ocado.com>
2018-05-21 17:43:51 +0100

    Finished highest elo with 77% accuracy on data-standard/*

--- highest-elo.py
+++ highest-elo.py
@@ -1,6 +1,6 @@
 import csv
 
-
+#Read data from csv files
 x_test = []
 with open('data-standard/x_test.csv','rb') as csvfile:
 	reader = csv.reader(csvfile)
@@ -13,22 +13,19 @@ with open('data-standard/y_test.csv','rb') as csvfile:
 	for row in reader:
 		y_test.append(row)
 
+#Variables for calculating accuracy
 game_count = len(x_test)
 correct_count = 0.0
 
+#for every game in the test set, predict -1 or 1
 for i in range(len(x_test)):
-	if( int(x_test[i][2]) < int(x_test[i][3]) - 20 ):
-		if(y_test[i][0] == '-1'):	
-			correct_count +=1
-		print -1, y_test[i]
-	elif( int(x_test[i][2]) > int(x_test[i][3]) + 20 ):
-		if(y_test[i][0] == '1'):
-			correct_count += 1	
-		print 1, y_test[i]
-	else:
-		if(y_test[i][0] == '0'):
-			correct_count += 1	
-		print 0, y_test[i]
-	
+    if( x_test[i][2] >= x_test[i][3]  ):
+        if(y_test[i][0] == '1'):
+            correct_count += 1
+        print '1', y_test[i]
+    else:
+        if( y_test[i][0] == '-1' ):
+            correct_count += 1
+        print '-1', y_test[i]
 
 print (correct_count/game_count)*100
